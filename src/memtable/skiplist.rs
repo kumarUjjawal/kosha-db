0868ed50ccbf225fc9d527ef07edce173b73004e
Kumar Ujjawal <ujjawalpathak6@gmail.com>
2026-04-02 18:02:21 +0530

    implemented skiplist

--- src/memtable/skiplist.rs
+++ src/memtable/skiplist.rs
@@ -60,4 +60,140 @@ impl<C: Comparator> SkipListMemTable<C> {
             rng_state: 0x9E37_79B9_7F4A_7C15,
         }
     }
+
+    fn validate_key(key: &[u8]) -> Result<()> {
+        if key.is_empty() {
+            return Err(DbError::EmptyKey);
+        }
+        Ok(())
+    }
+
+    fn next_index(&self, current: Option<usize>, level: usize) -> Option<usize> {
+        match current {
+            None => self.head[level],
+            Some(index) => self.nodes[index].next[level],
+        }
+    }
+
+    fn set_next(&mut self, current: Option<usize>, level: usize, next: Option<usize>) {
+        match current {
+            None => self.head[level] = next,
+            Some(index) => self.nodes[index].next[level] = next,
+        }
+    }
+
+    fn find_preprocessor(&self, key: &[u8]) -> [Option<usize>; MAX_HEIGHT] {
+        let mut update = [None; MAX_HEIGHT];
+        let mut current = None;
+
+        for level in (0..self.height).rev() {
+            loop {
+                let Some(next_index) = self.next_index(current, level) else {
+                    break;
+                };
+
+                match self
+                    .comparator
+                    .compare(self.nodes[next_index].key.as_slice(), key)
+                {
+                    Ordering::Less => current = Some(next_index),
+                    Ordering::Equal | Ordering::Greater => break,
+                }
+            }
+
+            update[level] = current;
+        }
+        update
+    }
+
+    fn find_existing_index(
+        &self,
+        key: &[u8],
+        update: &[Option<usize>; MAX_HEIGHT],
+    ) -> Option<usize> {
+        let next = self.next_index(update[0], 0)?;
+
+        match self
+            .comparator
+            .compare(self.nodes[next].key.as_slice(), key)
+        {
+            Ordering::Equal => Some(next),
+            Ordering::Less | Ordering::Greater => None,
+        }
+    }
+
+    fn next_random(&mut self) -> u64 {
+        let mut value = self.rng_state;
+        value ^= value << 13;
+        value ^= value >> 7;
+        value ^= value << 17;
+        self.rng_state = value;
+        value
+    }
+
+    fn random_height(&mut self) -> usize {
+        let mut height = 1;
+
+        while height < MAX_HEIGHT && (self.next_random() & 0b11) == 0 {
+            height += 1;
+        }
+
+        height
+    }
+
+    fn insert_entry(&mut self, key: Key, value: EntryValue) -> Result<()> {
+        Self::validate_key(&key)?;
+
+        let mut update = self.find_preprocessor(&key);
+
+        if let Some(existing_index) = self.find_existing_index(&key, &update) {
+            self.nodes[existing_index].value = value;
+            return Ok(());
+        }
+
+        let node_height = self.random_height();
+
+        if node_height > self.height {
+            for slot in update.iter_mut().take(node_height).skip(self.height) {
+                *slot = None;
+            }
+            self.height = node_height;
+        }
+
+        let mut node = Node::new(key, value, node_height);
+
+        for (level, next_slot) in node.next.iter_mut().enumerate() {
+            *next_slot = self.next_index(update[level], level);
+        }
+
+        let new_index = self.nodes.len();
+        self.nodes.push(node);
+
+        for (level, predecessor) in update.iter().copied().take(node_height).enumerate() {
+            self.set_next(predecessor, level, Some(new_index));
+        }
+
+        self.len += 1;
+        Ok(())
+    }
+}
+
+impl<C: Comparator> MemTable for SkipListMemTable<C> {
+    fn put(&mut self, key: Key, value: Value) -> Result<()> {
+        self.insert_entry(key, EntryValue::Value(value))
+    }
+
+    fn delete(&mut self, key: Key) -> Result<()> {
+        self.insert_entry(key, EntryValue::Tombstone)
+    }
+
+    fn get(&mut self, key: &[u8]) -> Option<&EntryValue> {
+        let update = self.find_preprocessor(key);
+        let index = self.find_existing_index(key, &update)?;
+        Some(&self.nodes[index].value)
+    }
+
+    fn len(&self) -> usize {
+        self.len
+    }
 }
